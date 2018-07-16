c20386f1aaec5c7333d44b5c2a588fa193a2eaae
nekootoko3 <miyako.kerria@gmail.com>
2018-07-16 14:59:36 +0900

    add object-oriented binary_tree

--- section_8/binary_tree/binary_tree_3.py
+++ section_8/binary_tree/binary_tree_3.py
@@ -1,18 +1,23 @@
 N = int(input())
 
+class Node():
+    def __init__(self, parent = -1, left = -1, right = -1):
+        self.parent = parent
+        self.left = left
+        self.right = right
 
-binary_tree = [{"parent": -1} for _ in range(N)]
+binary_tree = [Node() for _ in range(N)]
 for _ in range(N):
     node_input = input()
     id, left, right = map(int, node_input.split())
 
-    binary_tree[id]["left"] = left
-    binary_tree[id]["right"] = right
+    binary_tree[id].left = left
+    binary_tree[id].right = right
 
     if left != -1:
-        binary_tree[left]["parent"] = id
+        binary_tree[left].parent = id
     if right != -1:
-        binary_tree[right]["parent"] = id
+        binary_tree[right].parent = id
 
 
 def set_depth(id, depth):
@@ -20,15 +25,15 @@ def set_depth(id, depth):
         return
 
     D[id] = depth
-    set_depth(binary_tree[id]["left"], depth+1)
-    set_depth(binary_tree[id]["right"], depth+1)
+    set_depth(binary_tree[id].left, depth+1)
+    set_depth(binary_tree[id].right, depth+1)
 
 def set_height(id):
     h1, h2 = 0, 0
-    if binary_tree[id]["left"] != -1:
-        h1 = set_height(binary_tree[id]["left"]) + 1
-    if binary_tree[id]["right"] != -1:
-        h2 = set_height(binary_tree[id]["right"]) + 1
+    if binary_tree[id].left != -1:
+        h1 = set_height(binary_tree[id].left) + 1
+    if binary_tree[id].right != -1:
+        h2 = set_height(binary_tree[id].right) + 1
     H[id] = max(h1, h2)
     return H[id]
 
@@ -36,7 +41,7 @@ def set_height(id):
 D = [0 for i in range(N)]
 H = [0 for i in range(N)]
 for id in range(N):
-    if binary_tree[id]["parent"] == -1:
+    if binary_tree[id].parent == -1:
         set_depth(id, 0)
         set_height(id)
         break
@@ -44,24 +49,24 @@ for id in range(N):
 
 
 def get_sibling(id):
-    parent_id = node["parent"]
+    parent_id = node.parent
     if parent_id == -1:
         return -1
-    if binary_tree[parent_id]["left"] != id:
-        return binary_tree[parent_id]["left"]
+    if binary_tree[parent_id].left != id:
+        return binary_tree[parent_id].left
     else:
-        return binary_tree[parent_id]["right"]
+        return binary_tree[parent_id].right
 
 def get_degree(node):
     degree = 0
-    if node["left"] != -1:
+    if node.left != -1:
         degree += 1
-    if node["right"] != -1:
+    if node.right != -1:
         degree += 1
     return degree
 
 def get_type(node, dgree):
-    if node["parent"] == -1:
+    if node.parent == -1:
         return "root"
     if degree == 0:
         return "leaf"
@@ -69,7 +74,7 @@ def get_type(node, dgree):
 
 for id in range(N):
     node = binary_tree[id]
-    parent_id = node["parent"]
+    parent_id = node.parent
     sibling_id = get_sibling(id)
     degree = get_degree(node)
     node_type = get_type(node, degree)
